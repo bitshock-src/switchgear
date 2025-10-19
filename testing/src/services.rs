d24a7580988f018d61f7ed4715120634dc66b24a
curtis lee fulton <curtis@ciphx.com>
2025-10-19 11:09:53 -0700

    ci: implement pipeline #33

--- testing/src/services.rs
+++ testing/src/services.rs
@@ -17,21 +17,24 @@ impl IntegrationTestServices {
         dotenvy::from_path(&services_env_file)
             .with_context(|| format!("loading .env file {}", services_env_file.display()))?;
 
-        let postgres_hostname = env::var("POSTGRES_HOSTNAME")?;
+        let postgres_hostname = format!("services_network.{}", env::var("POSTGRES_HOSTNAME")?);
         let postgres_port = env::var("POSTGRES_PORT")?.parse::<u16>()?;
         let postgres = match postgres_hostname.to_socket_addrs() {
             Ok(_) => format!("{postgres_hostname}:{postgres_port}"),
             Err(_) => format!("localhost:{postgres_port}"),
         };
 
-        let mysql_hostname = env::var("MYSQL_HOSTNAME")?;
+        let mysql_hostname = format!("services_network.{}", env::var("MYSQL_HOSTNAME")?);
         let mysql_port = env::var("MYSQL_PORT")?.parse::<u16>()?;
         let mysql = match mysql_hostname.to_socket_addrs() {
             Ok(_) => format!("{mysql_hostname}:{mysql_port}"),
             Err(_) => format!("localhost:{mysql_port}"),
         };
 
-        let credentials_hostname = env::var("CREDENTIALS_SERVER_HOSTNAME")?;
+        let credentials_hostname = format!(
+            "services_network.{}",
+            env::var("CREDENTIALS_SERVER_HOSTNAME")?
+        );
         let credentials_port = env::var("CREDENTIALS_SERVER_PORT")?.parse::<u16>()?;
         let credentials = match credentials_hostname.to_socket_addrs() {
             Ok(_) => format!("{credentials_hostname}:{credentials_port}"),
@@ -57,7 +60,6 @@ impl IntegrationTestServices {
         &self.credentials
     }
 }
-
 
 #[cfg(test)]
 mod tests {
