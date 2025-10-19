21a745adc67e6527bf7ef565981d3c4aabd6d892
curtis lee fulton <curtis@ciphx.com>
2025-10-19 10:29:11 -0700

    ci: implement pipeline #33

--- testing/src/services.rs
+++ testing/src/services.rs
@@ -1,11 +1,13 @@
 use anyhow::Context;
 use std::env;
+use std::net::ToSocketAddrs;
 use std::path::PathBuf;
 
 #[derive(Debug)]
 pub struct IntegrationTestServices {
     postgres: String,
     mysql: String,
+    credentials: String,
 }
 
 impl IntegrationTestServices {
@@ -15,23 +17,32 @@ impl IntegrationTestServices {
         dotenvy::from_path(&services_env_file)
             .with_context(|| format!("loading .env file {}", services_env_file.display()))?;
 
-        // let postgres_hostname = env::var("POSTGRES_HOSTNAME")?;
+        let postgres_hostname = env::var("POSTGRES_HOSTNAME")?;
         let postgres_port = env::var("POSTGRES_PORT")?.parse::<u16>()?;
-        // let postgres = match postgres_hostname.to_socket_addrs() {
-        //     Ok(_) => format!("{postgres_hostname}:{postgres_port}"),
-        //     Err(_) => format!("localhost:{postgres_port}"),
-        // };
-        let postgres = format!("localhost:{postgres_port}");
+        let postgres = match postgres_hostname.to_socket_addrs() {
+            Ok(_) => format!("{postgres_hostname}:{postgres_port}"),
+            Err(_) => format!("localhost:{postgres_port}"),
+        };
 
-        // let mysql_hostname = env::var("MYSQL_HOSTNAME")?;
+        let mysql_hostname = env::var("MYSQL_HOSTNAME")?;
         let mysql_port = env::var("MYSQL_PORT")?.parse::<u16>()?;
-        // let mysql = match mysql_hostname.to_socket_addrs() {
-        //     Ok(_) => format!("{mysql_hostname}:{mysql_port}"),
-        //     Err(_) => format!("localhost:{mysql_port}"),
-        // };
-        let mysql = format!("localhost:{mysql_port}");
+        let mysql = match mysql_hostname.to_socket_addrs() {
+            Ok(_) => format!("{mysql_hostname}:{mysql_port}"),
+            Err(_) => format!("localhost:{mysql_port}"),
+        };
 
-        Ok(Self { postgres, mysql })
+        let credentials_hostname = env::var("CREDENTIALS_SERVER_HOSTNAME")?;
+        let credentials_port = env::var("CREDENTIALS_SERVER_PORT")?.parse::<u16>()?;
+        let credentials = match credentials_hostname.to_socket_addrs() {
+            Ok(_) => format!("{credentials_hostname}:{credentials_port}"),
+            Err(_) => format!("localhost:{credentials_port}"),
+        };
+
+        Ok(Self {
+            postgres,
+            mysql,
+            credentials,
+        })
     }
 
     pub fn postgres(&self) -> &str {
@@ -40,5 +51,9 @@ impl IntegrationTestServices {
 
     pub fn mysql(&self) -> &str {
         &self.mysql
+    }
+
+    pub fn credentials(&self) -> &str {
+        &self.credentials
     }
 }
